d21fc15e75c4b682cbb643e850217317472acae8
Namita Sharma <namitasharma9553@gmail.com>
2020-12-28 19:55:07 +0530

    Update chatbot.cpp

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -42,11 +42,61 @@ ChatBot::~ChatBot()
     }
 }
 
-//// STUDENT CODE
-////
+ChatBot::ChatBot(const ChatBot &source)
+{
 
-////
-//// EOF STUDENT CODE
+    std::cout << "ChatBot Copy Constructor" << std::endl;
+    _image = new wxBitmap(*source._image);
+	_chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+
+}
+
+ChatBot& ChatBot::operator=(const ChatBot &source)
+{
+    std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+
+    if (&source == this)
+        return *this;
+	else{
+		_image = new wxBitmap(*source._image);
+		_chatLogic = source._chatLogic;
+		_rootNode = source._rootNode;
+		return *this;
+	}
+}
+
+ChatBot::ChatBot(ChatBot &&source) 
+{
+    std::cout << "ChatBot Move Contructor" << std::endl;
+    _image = source._image;
+	_chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    _chatLogic->SetChatbotHandle(this);    
+    source._image = NULL;
+	source._chatLogic = NULL;
+    source._rootNode = NULL;
+    
+}
+
+ChatBot& ChatBot::operator=(ChatBot &&source) // Move assignment
+{
+    std::cout << "ChatBot Move Assignment Operator" << std::endl;
+
+    if (&source == this)
+        return *this;
+	else {
+		if (_image==NULL) 
+			delete _image;
+		_image = source._image;
+		_chatLogic = source._chatLogic;
+		_rootNode = source._rootNode;
+		_chatLogic->SetChatbotHandle(this);    
+		source._chatLogic = NULL;
+		source._rootNode = NULL;
+		source._image = NULL;
+	}
+}
 
 void ChatBot::ReceiveMessageFromUser(std::string message)
 {
